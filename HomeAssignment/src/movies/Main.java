ddf07abf6a4fc4da47963b4ad43529cbceffc178
Barel3220 <barelhatuka@gmail.com>
2021-12-30 17:03:13 +0200

    Getting user input in loop

--- HomeAssignment/src/movies/Main.java
+++ HomeAssignment/src/movies/Main.java
@@ -1,10 +1,20 @@
 package movies;
 
+import java.util.Scanner;
+
 public class Main {
-
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner userScanner = new Scanner(System.in);
 
+		while (true) {
+			System.out.println("Please enter an imdb_id: ");
+			// getting the user next ID
+			String imdbID = userScanner.nextLine();
+
+			// checking if the user wants to end the session
+			if (imdbID.contains("0"))
+				return;
+		}
 	}
-
 }
